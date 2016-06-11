bf24820d89bd1a8281a26346002eb20cbee39e4c
Grant Hutchison <Stiflertime@gmail.com>
2016-06-11 23:55:44 +0100

    (empty) ControllerUnitTest class created

---
+++ app/src/test/java/com/granthutchison/stifdev_tap/ControllerUnitTests.java
@@ -0,0 +1,16 @@
+package com.granthutchison.stifdev_tap;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * A junit test file for the Controller class
+ * Created by Stifler on 11/06/2016.
+ */
+public class ControllerUnitTests {
+
+    @Test
+
+
+}
