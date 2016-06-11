35f24f68973f6ea003dd4987642cf9bef1c4a827
Grant Hutchison <Stiflertime@gmail.com>
2016-06-11 17:17:50 +0100

    implemented the Item class

--- app/src/main/java/com/granthutchison/stifdev_tap/Model/Item.java
+++ app/src/main/java/com/granthutchison/stifdev_tap/Model/Item.java
@@ -1,6 +1,11 @@
 package com.granthutchison.stifdev_tap.Model;
 
 /**
+ * The Item class represents an item which can be used in the game.
+ * It holds details of whether it has already been used, along with details of text to display if
+ * it is successfully used, or an unsuccessful attempt is made. Copies of the Item object will be
+ * made available to the GUI by way of the Scenario classes inventory attribute, therefore getter
+ * methods are made public.
  * Created by Stifler on 08/06/2016.
  */
 public class Item {
@@ -9,8 +14,18 @@ public class Item {
     private Boolean used;
     private String unlocks; //The String title of the room it unlocks
 
-    public Item(String name) {
+    private String usedText; //The description to be displayed when the item is used - perhaps should be a parameter in Room?
+    private String failText; //The text to be displayed when an attempt is made to use the item but fails.
+
+
+    public Item(String name, String unlocks, String usedText, String failText) {
         this.name = name;
         this.used = false;
+        this.unlocks = unlocks;
+        this.usedText = usedText;
+        this.failText = failText;
     }
+
+
+
 }
