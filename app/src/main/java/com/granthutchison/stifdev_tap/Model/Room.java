82ce8b3ff5a729c758b54da04527c3b2c086abff
Grant Hutchison <Stiflertime@gmail.com>
2016-06-11 16:23:17 +0100

    getRoomDescription method in Room class implemented.

--- app/src/main/java/com/granthutchison/stifdev_tap/Model/Room.java
+++ app/src/main/java/com/granthutchison/stifdev_tap/Model/Room.java
@@ -1,4 +1,9 @@
 /**
+ * Room objects represent rooms/areas in the text adventure.
+ * Each Room has a title (e.g. "Entrance Hall") and description (e.g. "You are standing in a large entrance hallway").
+ * Each room can have up to four exits ((Top, Bottom, Right, Left) any of which can be locked.
+ * If all exits of a room are unlocked then an updated description is held to display (the current
+ * solution only really allows for one locked door).
  * Created by Stifler on 08/06/2016.
  */
 
@@ -8,31 +13,31 @@ public class Room {
 
 
     private String title;
-    private String containsItem = ""; //TODO: Implement Item class.
+    private String containsItem = "";
     //Do we need this attribute?
 
     //Room descriptions
-    private String initialDescription; //An initial description displayed for the room.
-    private String itemCollectedDescription; //An updated description if an item is picked up in the room.
+    private String roomLockedDescription; //An initial description displayed for the room.
+    private String itemCollectedDescription; //An updated description if an item is picked up in the room. - may not be required.
     private String roomsUnlockedDescription; //An updated description if all adjacent rooms have been unlocked.
 
     //Adjacent Rooms and the lock status of the 'door'.
     private String top;
-    private Boolean topLocked;
+    private boolean topLocked;
 
     private String bottom;
-    private Boolean bottomLocked;
+    private boolean bottomLocked;
 
     private String left;
-    private Boolean leftLocked;
+    private boolean leftLocked;
 
     private String right;
-    private Boolean rightLocked;
+    private boolean rightLocked;
 
-    public Room(String title, String containsItem, String initialDescription, String itemCollectedDescription, String roomsUnlockedDescription, String top, Boolean topLocked, String bottom, Boolean bottomLocked, String left, Boolean leftLocked, String right, Boolean rightLocked) {
+    public Room(String title, String containsItem, String initialDescription, String itemCollectedDescription, String roomsUnlockedDescription, String top, boolean topLocked, String bottom, boolean bottomLocked, String left, boolean leftLocked, String right, boolean rightLocked) {
         this.title = title;
         this.containsItem = containsItem;
-        this.initialDescription = initialDescription;
+        this.roomLockedDescription = initialDescription;
         this.itemCollectedDescription = itemCollectedDescription;
         this.roomsUnlockedDescription = roomsUnlockedDescription;
         this.top = top;
@@ -51,6 +56,22 @@ public class Room {
      */
     protected String getTitle(){
         return this.title;
+    }
+
+    /**
+     * getRoomDescription determines if any of the exits in the room are locked.
+     * If any are locked then the roomLockedDescription is returned
+     * If all rooms are unlocked then the roomUnlockedDescription is returned.
+     * @return The current valid description of the room
+     */
+
+    protected String getRoomDescription(){
+        if(topLocked || bottomLocked || leftLocked || rightLocked){
+            return this.roomLockedDescription;
+        }else{
+            return this.roomsUnlockedDescription;
+        }
+
     }
 
 
