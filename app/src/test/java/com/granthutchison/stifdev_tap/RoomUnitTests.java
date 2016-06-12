779907989c1bb864b09165d58cc1056960f24864
Grant Hutchison <Stiflertime@gmail.com>
2016-06-12 23:13:21 +0100

    fixed Room unit tests following refactor

--- app/src/test/java/com/granthutchison/stifdev_tap/RoomUnitTests.java
+++ app/src/test/java/com/granthutchison/stifdev_tap/RoomUnitTests.java
@@ -13,8 +13,8 @@ import static org.junit.Assert.*;
  * Created by Stifler on 11/06/2016.
  */
 public class RoomUnitTests {
-    Room unlockedRoom = new Room("Hall", "Hammer", "The door is locked", "You picked up the Hammer","The doors are unlocked", "Room1", false, "Room2", false, "Room3", false, "Room4", false);
-    Room lockedRoom = new Room("Hall", "Hammer", "The door is locked", "You picked up the Hammer","The doors are unlocked", "Room1", false, "Room2", true, "Room3", false, "Room4", false);
+    Room unlockedRoom = new Room("Hall", "Hammer", "The door is locked", "You picked up the Hammer","The doors are unlocked", "North","Room1", false, "South","Room2", false, "West","Room3", false,"East", "Room4", false);
+    Room lockedRoom = new Room("Hall", "Hammer", "The door is locked", "You picked up the Hammer","The doors are unlocked", "North","Room1", false, "South","Room2", true, "West","Room3", false, "East","Room4", false);
 
     @Test
     public void roomToString(){
