c5ab8dd7902d3040604eb66179b8413cf7d50b6e
Grant Hutchison <Stiflertime@gmail.com>
2016-07-07 13:14:54 +0100

    Work done to add delays to credits - further bugfixes required

--- app/src/main/java/com/granthutchison/stifdev_tap/Activities/CreditsActivity.java
+++ app/src/main/java/com/granthutchison/stifdev_tap/Activities/CreditsActivity.java
@@ -1,9 +1,12 @@
 package com.granthutchison.stifdev_tap.Activities;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.granthutchison.stifdev_tap.Model.Controller;
@@ -27,6 +30,8 @@ public class CreditsActivity extends AppCompatActivity {
     private TextView creditsView4;
     private TextView creditsView5;
     private LinkedHashMap<String,String> endCredits;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,38 +63,73 @@ public class CreditsActivity extends AppCompatActivity {
         TextView currentView;
         TextView previousView = null;
         List<TextView> viewList = new ArrayList<>();
+
+        //Local variables to hold the credit text
+        String creditHeader;
+        String creditBody;
+
+        //Add each of the views to the global list to allow iteration
         viewList.add(creditsView1);
         viewList.add(creditsView2);
         viewList.add(creditsView3);
         viewList.add(creditsView4);
         viewList.add(creditsView5);
+
         //Iterate over the map of credits
+
         for (Map.Entry<String, String> credit : endCredits.entrySet()) {
-            //reset the counter if it reaches for
-            if(counter == 4){
+            //reset the counter if it reaches four - only 5 on-screen views
+            if(counter > 4){
                 counter = 0;
             }
             currentView = viewList.get(counter);
+            Log.d("rollCredits","View number "+counter+" being processed");
+            //Store the values for the credits as local variables
+            creditHeader = credit.getKey();
+            creditBody = credit.getValue();
 
             //If a previous view has been populated, blank it
-            if(previousView != null){
-                //TODO: Replace this with a fade out animation
-                previousView.setText("");
-            }
+            clearPreviousView(previousView);
+            //Write the credits to the current view
+            writeCredits(currentView, creditHeader, creditBody);
 
-            //TODO: Add fade in animation to the getKey line
-            currentView.setText(credit.getKey());
-            currentView.append("\n");
-            //Add a time delay
-
-            //TODO: Write code to write the characters on screen one by one
-            currentView.append(credit.getValue());
             counter++;
             previousView = currentView;
+
+
         }
 
         //TODO: Replace this snackbar with the ability to end the game
         Snackbar.make(creditsView1, "The credits have ended!",Snackbar.LENGTH_LONG).show();
+    }
+
+    private void clearPreviousView(TextView previousView){
+        if(previousView != null){
+            //TODO: Replace this with a fade out animation
+            previousView.setText("");
+            Log.d("rollCredits","Previous view being cleared down: "+previousView.toString());
+        }
+    }
+
+    private void writeCredits(final TextView currentView, String creditHeader, final String creditBody){
+        //TODO: Add fade in animation to the creditHeader line
+        currentView.setText(creditHeader);
+        currentView.append("\n");
+        //Add a time delay
+        //Initial 2 second delay before credits body is written, then a further 2 second delay before
+        //The counter ends.
+        new CountDownTimer(4000, 2000) {
+
+            public void onTick(long millisUntilFinished) {
+                //TODO: Write code to write the characters on screen one by one
+                currentView.append(creditBody);
+            }
+
+            public void onFinish() {
+                //Do nothing yet.
+            }
+        }.start();
+
     }
 
 }
