60a49b33c006e16a485837149c3cfde0f67c3c6d
Grant Hutchison <Stiflertime@gmail.com>
2016-07-24 23:23:38 +0100

    Updates to info page

--- app/src/main/java/com/granthutchison/stifdev_tap/Activities/AboutDialog.java
+++ app/src/main/java/com/granthutchison/stifdev_tap/Activities/AboutDialog.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.granthutchison.stifdev_tap.R;
+import com.granthutchison.stifdev_tap.Util.FontManager;
 
 /**
  * Created by Stifler on 23/07/2016.
@@ -26,6 +27,7 @@ public class AboutDialog extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.about_dialog,null);
         builder.setView(dialogView);
+        //Get the close button and set it up to close the dialog
         Button btnClose = (Button) dialogView.findViewById(R.id.btnAboutClose);
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,14 @@ public class AboutDialog extends DialogFragment {
             }
         });
 
+        Button btnAbtExpand = (Button) dialogView.findViewById(R.id.btnAbtExpand);
+
+        //Use the custom utility to set FontAwesome fonts on buttons
+        btnAbtExpand.setTypeface(FontManager.getTypeface(btnAbtExpand.getContext(),FontManager.FONTAWESOME));
+        btnAbtExpand.setText(R.string.fa_icon_expand);
+
+
+        //Set the style - this is configured to set the width of the box
         builder.getContext().getTheme().applyStyle(R.style.About_Dialog, true);
 
         return builder.create();
