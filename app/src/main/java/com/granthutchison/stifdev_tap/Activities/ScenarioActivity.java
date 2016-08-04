b440b0466295f059dcbd698f111c76a7919b35ff
Grant Hutchison <Stiflertime@gmail.com>
2016-08-04 22:53:31 +0100

    Scenario select updated with tutorial and arklay links

--- app/src/main/java/com/granthutchison/stifdev_tap/Activities/ScenarioActivity.java
+++ app/src/main/java/com/granthutchison/stifdev_tap/Activities/ScenarioActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.granthutchison.stifdev_tap.Model.Controller;
@@ -11,7 +12,8 @@ import com.granthutchison.stifdev_tap.R;
 
 public class ScenarioActivity extends AppCompatActivity {
 
-    private TextView btnArkley;
+    private Button btnTutorial;
+    private Button btnArkley;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +21,34 @@ public class ScenarioActivity extends AppCompatActivity {
         final Controller myCont = Controller.getInstance();
         setContentView(R.layout.scenario_select);
 
-        btnArkley = (TextView) findViewById(R.id.projectArkley);
+        btnTutorial = (Button) findViewById(R.id.btnTutorial);
+        btnArkley = (Button) findViewById(R.id.btnArklay);
+
+        btnTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myCont.startGame("Test");
+                startGame();
+            }
+        });
+
         btnArkley.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                myCont.startGame("Test");
-                Intent intent = new Intent(ScenarioActivity.this, RoomActivity.class);
-                startActivity(intent);
-
-
-
+                myCont.startGame("Arklay");
+                startGame();
             }
         });
+    }
+
+    /**
+     * The startGame() method in the ScenarioActivity class simply generates an Intent
+     * to start the RoomActivity. It should be called after the Controller.startGame()
+     * method has been called to generate the game map.
+     */
+    private void startGame(){
+        Intent intent = new Intent(ScenarioActivity.this, RoomActivity.class);
+        startActivity(intent);
     }
 }
